dff95c01d7c5ea9ad2b8772b7590d5d6953856bc
Carlos Carrasco Rodriguez <carlos.carrasco@gmail.com>
2020-07-24 08:35:28 -0400

    listas y scroll listos

--- lib/scr/routes/routes.dart
+++ lib/scr/routes/routes.dart
@@ -4,6 +4,8 @@ import 'package:componentes/scr/pages/avatar_pages.dart';
 import 'package:componentes/scr/pages/card_page.dart';
 import 'package:componentes/scr/pages/home_pages.dart';
 import 'package:componentes/scr/pages/input_page.dart';
+import 'package:componentes/scr/pages/listview_page.dart';
+import 'package:componentes/scr/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -14,5 +16,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage(),
   };
 }
